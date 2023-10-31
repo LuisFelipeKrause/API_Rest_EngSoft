4391cd80f8a10de694d69af4b32ec55dda024700
LuisFelipeKrause <lfelipe.krause@gmail.com>
2023-10-30 21:05:15 -0300

    Função base de listagem de itens finalizada. Conectada com index.php e UserController.php

--- api/src/Model/UserModel.php
+++ api/src/Model/UserModel.php
@@ -5,16 +5,29 @@
             require_once './Connection/ApiConnection.php';
             $conn = new ApiConnection();
             $conn = $conn->connect();
-            $query = "INSERT INTO usuarios (nome, cpf, email, senha) VALUES ($nome, $cpf, $email, $senha)";
-    
+            $query = "INSERT INTO usuarios (nome, cpf, email, senha) VALUES ('$nome', '$cpf', '$email', '$senha')";
+
             $resultado = mysqli_query($conn, $query);
-            
+            var_dump($resultado);
+            $result = mysqli_fetch_assoc($resultado);
+
             echo "Inserção feita com sucesso";
             $conn->close();
 
-            $result = mysqli_fetch_assoc($resultado);
-
             return $result;
+        }
+
+        function listarItens(){
+            require_once './Connection/ApiConnection.php';
+            $conn = new ApiConnection();
+            $conn = $conn->connect();
+
+            $query = "SELECT nome, cpf, email, senha FROM usuarios";
+
+            $resultado = mysqli_query($conn, $query);
+
+            $conn->close();
+            return $resultado;
         }
     }
 ?>
